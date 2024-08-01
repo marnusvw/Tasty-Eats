dbdfc0df662f0149e65890bea04a80425975cfa6
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-08-01 12:59:18 +0200

    Implement basic App component to fetch recipes.

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,39 @@
-import logo from '../logo.svg';
-import '../styles/App.css'
+import styles from '../styles/App.module.css'
+import Recipes from "../Data/data";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar/NavBar";
 
 function App() {
+  const [recipes, setRecipes] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchRandomRecipes = async () => {
+    console.log(isLoading);
+    const recipesFetched = await Recipes.getRecipes();
+    console.log(recipesFetched);
+    setRecipes(recipesFetched);
+    console.log(isLoading);
+  };
+  useEffect(() => {    
+    fetchRandomRecipes();
+    }, []);
+
+
+  
   return (
-    <></>
+    <div className={styles.App}>
+      <div className="NavBar">
+        <NavBar />
+      </div>
+      <ul>
+        {recipes.recipes?.map((recipe) => (
+          <li>
+            <img src={recipe.image} />
+            {recipe.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
