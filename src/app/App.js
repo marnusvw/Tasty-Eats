e703b33e359ad1b66286a1aecfe501dd75d211cc
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-08-13 12:57:23 +0200

    Updated Dynamic routes for a basic implementation.

--- src/app/App.js
+++ src/app/App.js
@@ -9,18 +9,33 @@ import {
 } from "react-router-dom";
 import Root from "../components/root/Root";
 import HomePage from "../pages/home/HomePage";
-import RecipesFilterPage from "../pages/recipes/RecipesFilterPage";
 import QuickRecipesPage from "../pages/quickRecipes/QuickRecipesPage";
 import SundayMealsPage from "../pages/sundayMeals/SundayMealsPage";
 import DietMealsPage from "../pages/dietMeals/DietMealsPage";
+import {getFilteredRecipes} from '../features/allRecipes/allRecipesSlice'
+import RecipePage from "../pages/recipePage/RecipePage";
 
-function App() {
+function App({ state, dispatch }) {
+  
+  const [selectedRecipe, setSelectedRecipe] = useState("");
+
+  const visibleAllRecipes = getFilteredRecipes(state.allRecipes, state.searchTerm);
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/RecipesPage" element={<RecipesFilterPage />} />
-        <Route path="RecipesPage/:type" element={<RecipesFilterPage />} />
+        <Route path="/" element={<HomePage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        {/*Base Routes*/}
+        <Route path="/RecipesPage" element={<HomePage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        <Route path="Quick and Easy" element={<QuickRecipesPage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        <Route path="Sunday Meals" element={<SundayMealsPage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        <Route path="Diet Meals" element={<DietMealsPage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+
+        {/* Dynamic Routes */}
+        <Route path="/RecipesPage/:type" element={<RecipePage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        <Route path="Quick and Easy/:type" element={<QuickRecipesPage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        <Route path="Sunday Meals/:type" element={<SundayMealsPage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
+        <Route path="Diet Meals/:type" element={<DietMealsPage visibleRecipes={visibleAllRecipes} dispatch={dispatch}/>} />
       </Route>
     )
   );
