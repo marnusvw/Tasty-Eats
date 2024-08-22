8a4a47fcd8be5e6361819048728676cd05c4ede6
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-08-22 12:57:12 +0200

    Refactor the recipe card.

--- src/components/Recipe/RecipeCard.js
+++ src/components/Recipe/RecipeCard.js
@@ -1,27 +1,27 @@
 import React from "react";
 import styles from "./RecipeCard.module.css";
-import DOMPurify from 'dompurify';
+
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import placeHolder from '../../assets/image_placeholder.png'
 
 
-const RecipeCard = ({recipe, onRecipeSelect }) => {
+const RecipeCard = ({recipe }) => {
   
-  const sanitizedHTML = DOMPurify.sanitize(recipe.summary);
+  
   const location = useLocation();
-  const navigate = useNavigate();
 
   const baseUrl = location.pathname === '/' ? 'RecipesPage/' : '';
   
   return (
-    <NavLink onClick={(recipe) => onRecipeSelect(recipe)} to={baseUrl + recipe.title}  className={styles.cardLink}>
+    <NavLink to={`${baseUrl} + ${recipe.title}`}>
        <div className={styles.recipeCard}>
     {recipe.image ? <img className={styles.recipeImage} src={recipe.image}/> : <img className={styles.recipeImage} src={placeHolder}/>}
       <h2>{recipe.title}</h2>
     <span className={styles.recipe_info}>
-    <h3>Servings: {recipe.servings}</h3>
-    <h3>Cook Time: {recipe.readyInMinutes} min</h3>
+    <h4>Servings: {recipe.servings}</h4>
+    <h4>Cook Time: {recipe.readyInMinutes} min</h4>
     </span>
+    <div>{recipe.dishTypes}</div>
    </div>
   </NavLink>
   )
